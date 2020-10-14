4f2762b14490864b55d30f289f7b1db9a97f3f9e
pcapriles <pedro.capriles@walmart.com>
2020-10-14 09:03:07 -0300

    Refactoring with SOLID Principle Aged Brie scope functionality

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,7 +7,6 @@ class GildedRose {
 
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    private static final int AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD = 0;
 
     private static final int BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 10;
 
@@ -16,10 +15,6 @@ class GildedRose {
     private static final int BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD = 0;
 
     private static final int DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD = 0;
-
-    private static final int MAX_QUALITY = 50;
-
-    private static final int MIN_QUALITY = 0;
 
     Item[] items;
 
@@ -30,10 +25,6 @@ class GildedRose {
     void updateQuality() {
         for (Item item : items) {
             switch (item.name) {
-                case AGED_BRIE:
-                    decreaseSellIn(item);
-                    updateAgedBrieQuality(item);
-                    break;
                 case BACKAGE_PASSES:
                     decreaseSellIn(item);
                     updateBackagePassesQuality(item);
@@ -45,19 +36,6 @@ class GildedRose {
                     updateDefaultItemQuality(item);
             }
         }
-    }
-
-    private void decreaseSellIn(Item item) {
-        item.sellIn -= 1;
-    }
-
-    private void updateAgedBrieQuality(Item item) {
-        increaseQuality(item);
-
-        if (item.sellIn < AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD) {
-            increaseQuality(item);
-        }
-
     }
 
     private void updateBackagePassesQuality(Item item) {
@@ -83,19 +61,4 @@ class GildedRose {
         }
     }
 
-    private void resetQuality(Item item) {
-        item.quality = 0;
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.quality < MAX_QUALITY) {
-            item.quality += 1;
-        }
-    }
-
-    private void descreaseQuality(Item item) {
-        if (item.quality > MIN_QUALITY) {
-            item.quality -= 1;
-        }
-    }
 }
